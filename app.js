c0c935dfeed24a56496e8ba4be2411eb7887b59a
Yousuff <yousufftanveer77@gmail.com>
2022-10-13 10:00:15 +0100

    completes 3 working tests for review id task 4

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getCategories } = require("./controller/controller");
+const { getCategories, getReviewById } = require("./controller/controller");
 const {
   handleInternalErrors,
   handleCustomErrors,
@@ -10,9 +10,14 @@ const app = express();
 app.use(express.json());
 
 app.get("/api/categories", getCategories);
+app.get("/api/reviews/:review_id", getReviewById);
 
 app.use(handleCustomErrors);
 
 app.use(handleInternalErrors);
+
+app.all("/*", (req, res) => {
+  res.status(404).send({ msg: "invalid endpoint" });
+});
 
 module.exports = app;
